6e3fe151b60edcfabf7847b13a579ab3752057bc
Jonathan Joelson <jonathan.joelson@accenture.com>
2019-09-17 16:08:10 -0400

    Update Package.swift to support iOS

--- Package.swift
+++ Package.swift
@@ -1,15 +1,18 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Charts",
+    platforms: [
+        .iOS(.v11)
+    ],
     products: [
-        .library(name: "Charts", type: .dynamic, targets: ["Charts"])
+        .library(name: "Charts", targets: ["Charts"])
     ],
     dependencies: [],
     targets: [
         .target(name: "Charts", dependencies: [])
     ],
-    swiftLanguageVersions: [5]
+    swiftLanguageVersions: [.v5]
 )
